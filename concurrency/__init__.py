d76b4e30190ed4d514455ddbb07cae96e6ed4d2e
Mohamed Eldesouki <disooqi@gmail.com>
2021-11-08 16:30:29 -0500

    Update __init__.py

--- concurrency/__init__.py
+++ concurrency/__init__.py
@@ -1,1 +1,6 @@
 # http://pybay.com/site_media/slides/raymond2017-keynote/index.html
+# https://www.youtube.com/watch?v=bckD_GK80oY
+# https://docs.python.org/3/library/asyncio.html
+# https://realpython.com/async-io-python/
+# https://realpython.com/python-concurrency/
+# https://realpython.com/python-async-features/
